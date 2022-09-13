10c0e9b91a9d6eb7e80783dfe212e1331e0282d9
maximgrinin <maximgrinin@gmail.com>
2022-09-13 11:47:29 +0300

    Tasks with star

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ db = SQLAlchemy(app)
 # Создаем API и неймспейсы
 api = Api(app)
 movies_ns = api.namespace('movies')
+directors_ns = api.namespace('directors')
+genres_ns = api.namespace('genres')
 
 
 # Модель для фильмов
@@ -82,14 +84,16 @@ directors_schema = DirectorSchema(many=True)
 
 # Функции API для фильмов - /movies/
 @movies_ns.route('/')
-class MovieView(Resource):
+class MoviesView(Resource):
     def get(self):
         genre_id = request.args.get("genre_id")
         director_id = request.args.get("director_id")
         # Обрабатываем возможность получения жанра в параметрах запроса /movies/?genre_id=1
         # Обрабатываем возможность получения режиссера в параметрах запроса /movies/?director_id=1
         # Если никаких дополнительных параметров в запросе не получили - возвращаем все фильмы
-        if genre_id:
+        if genre_id and director_id:
+            query_result = db.session.query(Movie).filter(Movie.genre_id == genre_id, Movie.director_id == director_id).all()
+        elif genre_id:
             query_result = db.session.query(Movie).filter(Movie.genre_id == genre_id).all()
         elif director_id:
             query_result = db.session.query(Movie).filter(Movie.director_id == director_id).all()
@@ -110,7 +114,7 @@ class MovieView(Resource):
         return "", 201
 
 
-# Функции API для единичного экземпляра - фильма - /movies/<nt:uid>
+# Функции API для единичного экземпляра - фильма - /movies/<int:uid>
 @movies_ns.route('/<int:uid>')
 class MovieView(Resource):
     def get(self, uid):
@@ -149,6 +153,128 @@ class MovieView(Resource):
             db.session.commit()
         except Exception as e:
             return f'Фильм по указанному id = {uid} не найден - {str(e)}', 404
+
+
+# Функции API для режиссеров - /directors/
+@directors_ns.route('/')
+class DirectorsView(Resource):
+    def get(self):
+        query_result = db.session.query(Director).all()
+        # Если результаты запроса - пустые - говорим что данные не найдены
+        if not query_result:
+            return f'Результаты запроса не найдены', 404
+        return directors_schema.dump(query_result), 200
+
+    def post(self):
+        req_json = request.json
+        if not req_json:
+            return 'Ошибка при получении информации о новом режиссере', 404
+        new_director = Director(**req_json)
+        db.session.add(new_director)
+        db.session.commit()
+        return "", 201
+
+
+# Функции API для единичного экземпляра - режиссера - /directors/<int:uid>
+@directors_ns.route('/<int:uid>')
+class DirectorView(Resource):
+    def get(self, uid):
+        try:
+            director = db.session.query(Director).filter(Director.id == uid).one()
+            return director_schema.dump(director), 200
+        except Exception as e:
+            return f'Режиссер по указанному id = {uid} не найден - {str(e)}', 404
+
+    def put(self, uid):
+        try:
+            director = db.session.query(Director).filter(Director.id == uid).one()
+            req_json = request.json
+            [setattr(director, key, value) for key, value in req_json.items()]
+            db.session.add(director)
+            db.session.commit()
+            return "", 204
+        except Exception as e:
+            return f'Режиссер по указанному id = {uid} не найден - {str(e)}', 404
+
+    def patch(self, uid):
+        try:
+            director = db.session.query(Director).filter(Director.id == uid).one()
+            req_json = request.json
+            [setattr(director, key, value) for key, value in req_json.items()]
+            db.session.add(director)
+            db.session.commit()
+            return "", 204
+        except Exception as e:
+            return f'Режиссер по указанному id = {uid} не найден - {str(e)}', 404
+
+    def delete(self, uid):
+        try:
+            director = db.session.query(Director).filter(Director.id == uid).one()
+            db.session.delete(director)
+            db.session.commit()
+        except Exception as e:
+            return f'Режиссер по указанному id = {uid} не найден - {str(e)}', 404
+
+
+# Функции API для жанров - /genres/
+@genres_ns.route('/')
+class GenresView(Resource):
+    def get(self):
+        query_result = db.session.query(Genre).all()
+        # Если результаты запроса - пустые - говорим что данные не найдены
+        if not query_result:
+            return f'Результаты запроса не найдены', 404
+        return genres_schema.dump(query_result), 200
+
+    def post(self):
+        req_json = request.json
+        if not req_json:
+            return 'Ошибка при получении информации о новом жанре', 404
+        new_genre = Genre(**req_json)
+        db.session.add(new_genre)
+        db.session.commit()
+        return "", 201
+
+
+# Функции API для единичного экземпляра - жанра - /directors/<int:uid>
+@genres_ns.route('/<int:uid>')
+class DirectorView(Resource):
+    def get(self, uid):
+        try:
+            genre = db.session.query(Genre).filter(Genre.id == uid).one()
+            return genre_schema.dump(genre), 200
+        except Exception as e:
+            return f'Жанр по указанному id = {uid} не найден - {str(e)}', 404
+
+    def put(self, uid):
+        try:
+            genre = db.session.query(Genre).filter(Genre.id == uid).one()
+            req_json = request.json
+            [setattr(genre, key, value) for key, value in req_json.items()]
+            db.session.add(genre)
+            db.session.commit()
+            return "", 204
+        except Exception as e:
+            return f'Жанр по указанному id = {uid} не найден - {str(e)}', 404
+
+    def patch(self, uid):
+        try:
+            genre = db.session.query(Genre).filter(Genre.id == uid).one()
+            req_json = request.json
+            [setattr(genre, key, value) for key, value in req_json.items()]
+            db.session.add(genre)
+            db.session.commit()
+            return "", 204
+        except Exception as e:
+            return f'Жанр по указанному id = {uid} не найден - {str(e)}', 404
+
+    def delete(self, uid):
+        try:
+            genre = db.session.query(Genre).filter(Genre.id == uid).one()
+            db.session.delete(genre)
+            db.session.commit()
+        except Exception as e:
+            return f'Жанр по указанному id = {uid} не найден - {str(e)}', 404
 
 
 if __name__ == '__main__':
